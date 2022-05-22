1538804c06f68cc394363d100396c78c86596b94
Gfif <gfif90000@gmail.com>
2022-05-22 15:34:30 +0300

    arena pollish

---
+++ project/arena/enwarning.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+var count = 0
+var t = 0;
+func _physics_process(delta):
+	if count <0:
+		count = 0
+	if count == 0:
+		$Polygon2D.visible = false
+		t=0
+	else:
+		t+=delta*10
+		$Polygon2D.visible = true
+		$Polygon2D.color.r  = 1.0 + abs(sin(t)*0.2)
+
+func _on_enwarning_body_entered(body):
+	if body.is_in_group("enemies"):
+		count+=1
+
+func _on_enwarning_body_exited(body):
+	if body.is_in_group("enemies"):
+		count-=1
