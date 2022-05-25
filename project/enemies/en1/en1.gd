a93da8f00a43c219df913c22d8bbc91e81a4d919
Gfif <gfif90000@gmail.com>
2022-05-25 21:04:02 +0300

    working prototype

--- project/enemies/en1/en1.gd
+++ project/enemies/en1/en1.gd
@@ -4,7 +4,7 @@ var speed = 1
 var coldmg = 1
 func _physics_process(delta):
 	
-	rotation += delta
+	rotation += 2*delta
 	move_and_collide((Global.center - position).normalized()*speed)
 func damage(dmg):
 	hp-=dmg
--- project/enemies/parts/shield_1.gd
+++ project/enemies/parts/shield_1.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 var hp = 5
-
+var coldmg = 1
 func damage(dmg):
 	hp-=dmg
 	if hp<=0:
